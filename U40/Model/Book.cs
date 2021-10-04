e5d296912bf305e36031bf449bc726432ffacbfe
nguyentient2009a <tienndth2008051@fpt.edu.vn>
2021-10-04 19:05:40 +0700

    U37-U41

---
+++ U40/Model/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U40.Model
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string CoverImg { get; set; }
+    }
+    public class BookManager
+    {
+        public static List<Book> GetBooks()
+        {
+            var books = new List<Book>();
+
+            books.Add(new Book { BookId = 1, Title = "Vulpate", Author = "Futurum", CoverImg = "Assets/1.png" });
+            books.Add(new Book { BookId = 2, Title = "Mazim", Author = "Sequiter Que", CoverImg = "Assets/2.png" });
+            books.Add(new Book { BookId = 3, Title = "Elit", Author = "Tempor", CoverImg = "Assets/3.png" });
+
+            return books;
+        }
+    }
+}
---
+++ U41/Model/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U41.Model
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Avatar { get; set; }
+    }
+    public class Avatar
+    {
+        public string AvatarImage { get; set; }
+    }
+    public class ItemAvatar
+    {
+        public static List<Avatar> GetAvatars()
+        {
+            var avatars = new List<Avatar>();
+            avatars.Add(new Avatar { AvatarImage = "Assets/avatar.png" });
+            avatars.Add(new Avatar { AvatarImage = "Assets/avatar2.png" });
+            return avatars;
+        }
+    }
+}
